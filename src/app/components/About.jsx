68cc0719e5cb35449814bea19dcb4bdaee03bfb9
boss <75540581+Boss22341231@users.noreply.github.com>
2024-06-16 22:22:24 +0700

    third  commit

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -8,7 +8,7 @@ function About() {
         <Image priority={true} src="/aboutPic.png" width={400} height={400} alt='My pic' className="rounded-2xl border-solid border-8 border-sky-200 px-8 pt-8 mx-5" id='about_pic'></Image>
           <div>
             <h1 className="text-5xl font-bold">About me</h1>
-            <p className="py-6 text-xl">Hi, my name is Boss. I'm 17-year-old senior high school student from Thailand who wants to be a Font-End developer in the future. My hobbies are listening to music, watching movies, and coding. I will apply to university next year and I hope I can study in the Computer Science.</p>
+            <p className="py-6 text-xl">Hi, my name is Boss. I'm 17-year-old senior high school student from Thailand who wants to be a Full-Stack Developer in the future. My hobbies are listening to music, watching movies, and coding. I will apply to university next year and I hope I can study in the Computer Science.</p>
             <h2 className='text-3xl font-bold mb-7'>Skills</h2>
             <div className='grid lg:grid-cols-3 gap-5'>
               <div className="ProgrammingLanguages">
