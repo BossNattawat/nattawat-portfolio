04273cd137d888ce8213e73e5027834198829eed
boss <75540581+Boss22341231@users.noreply.github.com>
2024-07-16 12:31:11 +0700

    Updated Certificate.jsx

---
+++ src/app/data.jsx
@@ -0,0 +1,38 @@
+export default [
+    {
+      id: 0,
+      name: 'CS50X',
+      description: 'This course teaches me strong fundamentals in computer science.',
+      img:"/CS50x.jpg"
+    },
+    {
+      id: 1,
+      name: 'CS50B',
+      description: 'This course teaches me how to adapt technology to business.',
+      img:"/CS50B.png"
+    },
+    {
+      id: 2,
+      name: 'CS50S',
+      description: 'This course teaches me the scratch of programming by Scratch.',
+      img:"/CS50S.jpg"
+    },
+    {
+      id: 3,
+      name: 'SOLOLAERN Python',
+      description: 'This course teaches me the fundamentals of Python.',
+      img:"/python.png"
+    },
+    {
+      id: 4,
+      name: 'MU MOOC',
+      description: 'This course teaches me low-level programming languages like C language.',
+      img:"/C.png"
+    },
+    {
+      id: 5,
+      name: 'SQL Essential',
+      description: 'This course teaches me the essentials of SQL language.',
+      img:"/SQL.png"
+    }
+]
