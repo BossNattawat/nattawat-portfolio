b7b2e1f255d3887812775207e4e0cee611f8daf1
boss <75540581+Boss22341231@users.noreply.github.com>
2024-07-17 22:42:30 +0700

    Updated project.jsx main.jsx projectData.jsx

---
+++ src/app/components/Project.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Image from 'next/image'
+
+function Project({project}) {
+  return (
+    <div id='project'>
+        <div>
+            <h1 className='xl:text-5xl text-4xl font-bold text-center my-16'>My Projects</h1>
+        </div>
+        <div className="hero min-h-screen min-w-screen bg-base-200" data-theme="dim">
+            <div className="hero-content grid sm:grid-cols-2 lg:grid-cols-2 gap-10">
+                
+                {project.map((project)=>
+                    <div className="card card-compact bg-base-100 shadow-xl xl:w-[30rem] xl:h-[32rem]">
+                    <figure>
+                        <Image
+                            src={project.img}
+                            width={700}
+                            height={500}
+                            alt='Winston_Churchill'
+                            priority
+                        />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title font-bold mb-2">{project.title}</h2>
+                        <h6 className='mb-3 text-base'>Made with : {project.technologies}</h6>
+                        <p className='text-base pb-3'>{project.description}</p>
+                    </div>
+                </div>
+                )}
+                
+            </div>
+        </div>
+    </div>
+    
+  )
+}
+
+export default Project
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,17 +4,21 @@ import Main from './components/Main';
 import About from './components/About';
 import Contact from './components/Contact';
 import data from './data';
+import projectData from './projectData';
 import Certificate from './components/Certificate';
 import { useState } from 'react';
+import Project from './components/Project';
 
 export default function Home() {
 
   const [certificate, setCertificate] = useState(data)
+  const [project, setProject] = useState(projectData)
 
   return (
     <div data-theme="dim" className='bg-base-200'>
       <Main/>
       <About/>
+      <Project project={project}/>
       <Certificate certificate={certificate}/>
       <Contact/>
     </div>
