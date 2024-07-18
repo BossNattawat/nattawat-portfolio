2e2efad5e647ca668aad4dc954488cbf5f5731a0
boss <75540581+Boss22341231@users.noreply.github.com>
2024-07-18 23:25:26 +0700

    Updated Contact.jsx

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -5,8 +5,8 @@ function Contact() {
     <div className="hero min-h-96 bg-base-200" id='contact'>
         <div className="hero-content align-middle">
           <div className="max-w-md" id='contact_info'>
-            <h1 className="text-5xl font-bold">Contact</h1>
-            <h2 className="py-1 pt-8 text-xl">Email : nattawatsuktawe480@gmail.com</h2>
+            <h1 className="text-5xl font-bold my-8">Contact</h1>
+            {/* <h2 className="py-1 pt-8 text-xl">Email : nattawatsuktawe480@gmail.com</h2> */}
             <h2 className="py-1 text-xl">Tel : 061-242-9861</h2>
             <h2 className="py-1 text-xl">Instagram : nattawat_stw</h2>
           </div>
