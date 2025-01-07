83df8c4e71b7d9b6d7b2ccc9eb5b5545b2409b48
Nattawat <BossNattawat@users.noreply.github.com>
2025-01-07 14:12:31 +0700

    Update footer

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -8,7 +8,7 @@ function About() {
         <Image priority={true} src="/aboutPic.png" width={400} height={400} alt='My pic' className="rounded-2xl pt-8 mx-5 md:mb-8" id='about_pic'></Image> {/* border-solid border-8 border-sky-200 px-8 */}
           <div>
             <h1 className="text-5xl font-bold">About me</h1>
-            <p className="py-6 xl:text-xl sm:text-xl">Hi, my name is Boss. I'm a 17-year-old senior high school student from Thailand who wants to be a Full-Stack Developer in the future. My hobbies are listening to music, watching movies, and coding. I will apply to university next year and hope to study Computer Science.</p>
+            <p className="py-6 xl:text-xl sm:text-xl">Hi, my name is Boss. I'm a 18-year-old senior high school student from Thailand who wants to be a Full-Stack Developer in the future. My hobbies are listening to music, watching movies, and coding. I will apply to university this year and hope to study Computer Science.</p>
             <h2 className='text-3xl font-bold mb-7 sm:text-3xl'>Skills</h2>
             <div className='grid lg:grid-cols-3 lg:gap-5 md:gap-10'>
               <div className="pb-4">
@@ -27,7 +27,6 @@ function About() {
                   <li className='xl:text-lg ml-4 mt-1 sm:text-2xl'>Python</li>
                   <li className='xl:text-lg ml-4 mt-1 sm:text-2xl'>JavaScript</li>
                   <li className='xl:text-lg ml-4 mt-1 sm:text-2xl'>C language</li>
-                  <li className='xl:text-lg ml-4 mt-1 sm:text-2xl'>Java</li>
                 </ul>
               </div>
               
--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <div className="pt-48 xl:pt-4">
       <footer className="footer footer-center p-4 py-10 bg-base-300 text-base-content">
         <aside>
-            <p className="sm:text-xl lg:text-base">Copyright © 2024 - All right reserved by Nattawat</p>
+            <p className="sm:text-xl lg:text-base">Buid by <a href="https://github.com/BossNattawat" class='text-primary' target="_blank">BossNattawat</a></p>
         </aside>
       </footer>
     </div>
